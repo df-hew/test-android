6aa3740e911060d03af5f49e15f0195910fd9a23
hewcheehau <cheehau67@hotmail.com>
2025-05-06 17:21:59 +0800

    initial commit

---
+++ app/src/main/java/com/example/testcodebase/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.testcodebase
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dataflows.android.base.BaseScreen
+import com.dataflows.android.ui.widgets.PrimaryButton
+import com.example.testcodebase.ui.theme.TestCodeBaseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BaseScreen("hellos hew", showLoading = false)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TestCodeBaseTheme {
+        Greeting("Android")
+    }
+}
